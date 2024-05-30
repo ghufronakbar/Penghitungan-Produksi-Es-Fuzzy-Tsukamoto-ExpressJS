4ac28fb4e16b7392e6ee32d296543ed4b3edd5db
ghufron akbar <alan.040219.gam@gmail.com>
2024-05-30 21:09:12 +0700

    Vercel

--- connection.js
+++ connection.js
@@ -1,18 +1,18 @@
-const mysql = require("mysql");
-require('dotenv').config()
-const ENV = process.env
+// const mysql = require("mysql");
+// require('dotenv').config()
+// const ENV = process.env
 
-//koneksi database
-const conn = mysql.createConnection({
-  host: ENV.DB_HOST,
-  user: ENV.DB_USER,
-  password: ENV.DB_PASSWORD,
-  database: ENV.DB_NAME,
-});
+// //koneksi database
+// const conn = mysql.createConnection({
+//   host: ENV.DB_HOST,
+//   user: ENV.DB_USER,
+//   password: ENV.DB_PASSWORD,
+//   database: ENV.DB_NAME,
+// });
 
-conn.connect((err) => {
-  if (err) throw err;
-  console.log("MySQL Connected");
-});
+// conn.connect((err) => {
+//   if (err) throw err;
+//   console.log("MySQL Connected");
+// });
 
-module.exports = conn;
+// module.exports = conn;
